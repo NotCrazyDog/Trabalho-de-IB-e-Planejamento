9f49eca7679d5c8672eceea6dae12bfe39105b4d
NotCrazyDog <pedro.costa310@aluno.ce.gov.br>
2024-06-01 14:47:50 -0300

    Q1 modified

--- Q1.py
+++ Q1.py
@@ -1,4 +1,4 @@
-nome = input('Qual é o seu nome? ')
-sobrenome = input('Qual é o seu sobrenome? ')
+nome = input("Qual é o seu nome? ")
+sobrenome = input("Qual é o seu sobrenome? ")
 nomeCompleto = nome + " " + sobrenome
 print("Seu nome completo é: " + nomeCompleto)
