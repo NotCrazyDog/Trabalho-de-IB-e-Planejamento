f7fdce7fb1388134b6c3334f89d814625402b0a1
NotCrazyDog <pedro.costa310@aluno.ce.gov.br>
2024-06-01 14:51:05 -0300

    Q2 Added

---
+++ Q2.py
@@ -0,0 +1,9 @@
+varialOne = False
+varialTwo = "Hello World"
+varialThree = 1.618
+varialFour = 490
+
+print(f'A variável varialOne tem valor {varialOne} e é do tipo {type(varialOne)}')
+print(f'A variável varialTwo tem valor {varialTwo} e é do tipo {type(varialTwo)}')
+print(f'A variável varialThree tem valor {varialThree} e é do tipo {type(varialThree)}')
+print(f'A variável varialFour tem valor {varialFour} e é do tipo {type(varialFour)}')
